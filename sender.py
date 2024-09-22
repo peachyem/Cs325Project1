1923e7f10547d892d70d67659215eae88c5fe789
peachyem <emmobro@siue.edu>
2024-09-22 16:10:43 -0500

    responses now go to a different text file

--- sender.py
+++ sender.py
@@ -1,9 +1,15 @@
 import ollama
 
-file = open('prompts.txt', 'r')
-read = file.readlines()
+#open and read from prompt file
+promptfile = open('prompts.txt', 'r')
+read = promptfile.readlines()
+#holds the modified lines
 modified = []
+#keeps track of modified send to the ai
 count = 0
+
+#open the response file
+responsefile = open('responses.txt', 'w')
 
 #need to remove the /n from the end of the prompts
 for line in read:
@@ -11,8 +17,12 @@ for line in read:
 
 #respond to all
 for prompt in modified:
-   words = modified
-   response = ollama.generate(model="phi3", prompt=modified[count])
-   print(response)
+   responseO = ollama.generate(model="phi3", prompt=modified[count])
+   #specifically only get the response and put each one on a newline
+   responseClean = responseO["response"] + '\n'
+   print(responseClean)
+   #write to response file
+   responsefile.write(responseClean)
    count +=1
-
+#close the file
+responsefile.close()
